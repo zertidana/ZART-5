cdfc47f48743fb1de0318fd1d03f56e6f479bf76
zertidana <94858570+zertidana@users.noreply.github.com>
2024-08-15 19:35:38 +0100

    terrain

--- Assets/Scripts/MainMenu/SettingsMenu.cs
+++ Assets/Scripts/MainMenu/SettingsMenu.cs
@@ -16,22 +16,37 @@ public class SettingsMenu : MonoBehaviour
     public GameObject settingsMenuCanvas;
 
     void Start()
-    {
+    {/*
         resolutions = Screen.resolutions;
 
         resolutionDropdown.ClearOptions();
 
         List<string> options = new List<string>();
 
+        int currentResolutionIndex = 0;
+
         for (int i=0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + "x" + resolutions[i].height;
             options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
 
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();*/
     }
-    
+    /*
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        SetFullScreen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }*/
+
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("Volume", volume);
