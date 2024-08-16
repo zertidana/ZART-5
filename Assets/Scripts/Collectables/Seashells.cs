bfe93694a8f125b382ac9a8561a3ac747b442da0
zertidana <94858570+zertidana@users.noreply.github.com>
2024-08-16 07:08:55 +0100

    last

--- Assets/Scripts/Collectables/Seashells.cs
+++ Assets/Scripts/Collectables/Seashells.cs
@@ -8,14 +8,22 @@ public class Seashells : MonoBehaviour
     TMPro.TMP_Text text;
     int count;
 
+    private SceneController sceneController;
+
     void Awake ()
     {
         text = GetComponent<TMPro.TMP_Text>();
     }
 
-    void Start() => UpdateCount();
+    //void Start() => UpdateCount();
     void OnEnable() => Collectables.OnCollected += OnCollectableCollected;
     void OnDisable() => Collectables.OnCollected -= OnCollectableCollected;
+
+    public void Start()
+    {
+        UpdateCount();
+        sceneController = FindObjectOfType<SceneController>();
+    }
 
     void OnCollectableCollected()
     {
